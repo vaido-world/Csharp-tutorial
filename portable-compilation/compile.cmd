b4cc9410e56f42d1b8985d69914c081244dd9d97
Feldwor <BoQsc@users.noreply.github.com>
2020-04-17 01:09:19 +0300

    Create compile.cmd

---
+++ portable-compilation/compile.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+::Preseve Blank lines in For Loop https://stackoverflow.com/a/8812434
+:: For loop or find.exe might corrupt the output, this program is only for teaching and expected to have inaccuracy.
+SET "debug_output=%RANDOM%"
+
+
+FOR /F "delims=" %%A IN ('C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe test.cs ^| find /n /v ""') DO (
+
+  set "ln=%%A"
+  setlocal enableDelayedExpansion
+  set "ln=!ln:*]=!"
+  echo(!ln!
+  echo(!ln! >> "%debug_output%.txt"
+  endlocal
+)
+TIMEOUT 15
