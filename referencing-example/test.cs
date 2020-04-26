76e017450da291eb01b1c5575c5ac11d1039bcc3
Feldwor <BoQsc@users.noreply.github.com>
2020-04-26 19:56:37 +0300

    Add files via upload

---
+++ referencing-example/test.cs
@@ -0,0 +1,12 @@
+using System; 
+
+using OpenQA.Selenium;
+//using OpenQA.Selenium.PhantomJS;
+
+class HelloWorld {
+static void Main(string[] args) 
+    { 
+
+       Console.WriteLine("Hello World.");
+    }
+}
